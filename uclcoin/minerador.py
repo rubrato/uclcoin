68cab66cfa265b3ab800022c911dc11b5baa28e7
rubrato <45179696+rubrato@users.noreply.github.com>
2019-06-07 14:11:03 -0300

    Response Print
    só printando o retorno da mineração

--- uclcoin/minerador.py
+++ uclcoin/minerador.py
@@ -3,7 +3,7 @@ import requests
 import json
 from collections import namedtuple
 
-r = requests.get('http://127.0.0.1:5000/block/minable/0382499de4b7f5ffd6a86dd71c94364ba5f6682f7434304693f0df9130442dc072')
+r = requests.get('http://127.0.0.1:5000/block/minable/02f6a1813ae470f9c3c71eda29bbc711de17822946e87d84936eb3990ec4c74350')
 last_block = json.loads(r.text)
 block = Block.from_dict(last_block["block"])
 difficulty = last_block["difficulty"]
@@ -14,4 +14,5 @@ while block.current_hash[:difficulty].count('0') < difficulty:
 
 data = json.dumps(block, default=lambda x: x.__dict__)
 
-requests.post('http://127.0.0.1:5000/block',data,json=True)
+r = requests.post('http://127.0.0.1:5000/block',data,json=True)
+print(r.text)
